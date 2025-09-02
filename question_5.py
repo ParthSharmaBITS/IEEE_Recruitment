b6461c7aa9988ca4dcf925fac3a0ab1adc2a7297
ParthSharmaBITS <f20251087@pilani.bits-pilani.ac.in>
2025-09-02 21:37:03 +0530

    Added question_5 with improvements

--- question_5.py
+++ question_5.py
@@ -1,19 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m = 0
-sd = 1
-data = np.random.normal(loc=m, scale=sd, size=1000)
+m = 0 # mean
+sd = 1 # standard deviation
+data = np.random.normal(loc=m, scale=sd, size=1000) # generate 1000 random numbers from a normal distribution
 
-x = np.arange(1000) 
-plt.scatter(x, data, s=5)
+x = np.arange(1000) # x values from 0 to 999
+plt.scatter(x, data, s = 3) # scatter plot with point size 3
 
 plt.title("Scatter Plot of Normal Distribution")
 plt.xlabel("Index")
 plt.ylabel("Value")
 
-plt.grid(True)
-plt.show()
+plt.grid(True) # add grid
+plt.show() # display the plot
+
 
 
 
